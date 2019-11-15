84d6ad39abf15e1489d028b889ca30bf628b7f70
Sarah George <sarah@atomicrobot.com>
2019-11-15 14:10:20 -0500

    Initial Commit

---
+++ colorMatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  colorMatch
+//
+//  Created by Sarah on 11/15/19.
+//  Copyright © 2019 sgeorge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
